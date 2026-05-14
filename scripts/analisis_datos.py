54d401b602a83e71622cba61928a3cc4ed89c499
Paco (P2 - Desarrollador) <anatermignoni@gmail.com>
2026-05-14 02:41:56 +0000

    PROY-2: Agrega cálculo de anomalias

--- scripts/analisis_datos.py
+++ scripts/analisis_datos.py
@@ -20,3 +20,23 @@ except FileNotFoundError:
     print(f"Error: No se encontró el archivo en la ruta especificada.")
     print("Asegurate de que el archivo CSV esté dentro de la carpeta 'datos'.")
     exit()
+
+
+# Separamos las fuentes
+df_gistemp = df[df["Source"] == "GISTEMP"].sort_values("Year")
+df_gcag = df[df["Source"] == "GCAG"].sort_values("Year")
+
+# Indicadores GISTEMP
+prom_g = df_gistemp["Mean"].mean()
+max_g = df_gistemp["Mean"].max()
+min_g = df_gistemp["Mean"].min()
+anio_max_g = df_gistemp.loc[df_gistemp["Mean"].idxmax(), "Year"]
+anio_min_g = df_gistemp.loc[df_gistemp["Mean"].idxmin(), "Year"]
+
+# Indicadores GCAG
+prom_c = df_gcag["Mean"].mean()
+max_c = df_gcag["Mean"].max()
+min_c = df_gcag["Mean"].min()
+anio_max_c = df_gcag.loc[df_gcag["Mean"].idxmax(), "Year"]
+anio_min_c = df_gcag.loc[df_gcag["Mean"].idxmin(), "Year"]
+
